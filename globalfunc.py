4caef34b3381bdd0111170e25c77a39529625ee5
U-fafaly-PC\Administrator <fafaly@126.com>
2015-03-25 19:09:37 +0800

    1.22w

---
+++ globalfunc.py
@@ -0,0 +1,33 @@
+#!/bin/env python
+
+import os
+import sys
+
+fdatedir=''
+fname='tradingDates.csv'
+
+def getNextDate(cdate):
+	reader=open(fdatedir+fname,'r')
+	nextone=0
+	ndate=''
+	for line in reader:
+		if nextone==1:
+			ndate=line[0:8]
+			break
+		if line[0:8]==cdate:
+			nextone=1
+	return ndate
+
+def getLastDate(cdate):
+	reader=open(fdatedir+fname,'r')
+	nextone=0
+	ldate=reader.readline()
+	next(reader)
+	for line in reader:
+		if line[0:8]==cdate:
+			return ldate
+		else:
+			ldate=line[0:8]
+	return -1
+
+
