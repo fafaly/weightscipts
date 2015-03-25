16a518732a36ede78a3afc004cad100f0cdb7c0a
U-fafaly-PC\Administrator <fafaly@126.com>
2015-03-25 17:44:33 +0800

    1.12w

---
+++ runrange.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+
+import os
+import sys
+
+if len(sys.argv)<2:
+	print 'usage:./runrang.py [start date] [end date]'
+	exit()
+
+fd= open('tradingDates.csv','r')
+start=0
+for line in fd:
+	fdate=line[0:8]
+	if start==1:
+		os.system('./run.sh ' + fdate)
+	if fdate==sys.argv[1]:
+		start=1
+		os.system('./run.sh ' + fdate)
+	elif fdate==sys.argv[2]:
+		os.system('./run.sh ' + fdate)
+		break
